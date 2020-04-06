0342f5932429ae53725ea5e90e40da89aae96e61
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-06 20:38:51 +0300

    3

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {Navbar} from "./src/component/Navbar";
+import {AddTodo} from "./src/component/AddTodo";
+import {Todo} from "./src/component/Todo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Navbar title="Todo App" />
-    </View>
-  );
+    const [todos, setTodos] = useState([])
+
+    const addTodo = title => {
+        setTodos(prev => [...prev, {
+            id: Date.now().toString(),
+            title
+        }])
+    }
+    return (
+        <View>
+            <Navbar title="Todo App"/>
+            <View style={styles.container}>
+                <AddTodo onSubmit={addTodo}/>
+                <View>
+                    {todos.map(todo => <Todo key={todo.id} todo={todo}/>
+                    )}
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  }
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 10
+    }
 
 });
